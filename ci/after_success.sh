75da8277bf9e145879a4523f4c6a660d40cb6319
Jorge Aparicio <japaricious@gmail.com>
2016-07-22 18:34:08 -0500

    switch back to ghp-import

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -1,7 +1,7 @@
 set -ex
 
 if [[ "$TRAVIS_PULL_REQUEST" = 'false' && "$TRAVIS_OS_NAME" = "linux" ]]; then
-    ghp book
+    ./ghp-import/ghp_import.py book
     set +x
     git push -fq "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages && echo OK
     set -x
--- ci/install.sh
+++ ci/install.sh
@@ -1,13 +1,14 @@
 set -ex
 
 case $TRAVIS_OS_NAME in
+    linux)
+        git clone --depth 1 https://github.com/davisp/ghp-import
+        ;;
     osx)
         command -v xargo >/dev/null 2>&1 || cargo install xargo --debug
         brew install openocd qemu
     ;;
 esac
-
-command -v ghp >/dev/null 2>&1 || cargo install ghp --debug
 
 # TODO use versioned release instead of git snapshot
 command -v mdbook >/dev/null 2>&1 || cargo install --git https://github.com/azerupi/mdbook --debug --no-default-features --features output
