a298a36300d8bfda0f4bbbc438c6224de2df1d88
Jorge Aparicio <japaricious@gmail.com>
2016-07-22 18:13:02 -0500

    get rid of robots.txt

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -1,7 +1,6 @@
 set -ex
 
 if [[ "$TRAVIS_PULL_REQUEST" = 'false' && "$TRAVIS_OS_NAME" = "linux" ]]; then
-    echo "User-agent: *\nDisallow: /" > book/robots.txt
     ghp book
     set +x
     git push -fq "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages && echo OK
