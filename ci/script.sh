fba9f3620447e8e8adbc6ae7838b2178c5afc83b
Jorge Aparicio <japaricious@gmail.com>
2016-07-21 22:21:37 -0500

    ci: also test on macOS

--- ci/script.sh
+++ ci/script.sh
@@ -1,18 +1,21 @@
 set -ex
 
 build_apps() {
-    local tag=2016-05-10
     local target=cortex-m3
 
-    # The copper user has id = 1000, but this may not match the travis user id. To workaround this
-    # issue, make everything world write-able.
-    chmod -R a+w .
+    case $TRAVIS_OS_NAME in
+        linux)
+            local tag=2016-05-10
 
-    docker run \
-           -v $(pwd):/mnt \
-           -w /mnt \
-           japaric/copper:$tag \
-           bash -ex -c "
+            # The copper user has id = 1000, but this may not match the travis user id. To workaround this
+            # issue, make everything world write-able.
+            chmod -R a+w .
+
+            docker run \
+                   -v $(pwd):/mnt \
+                   -w /mnt \
+                   japaric/copper:$tag \
+                   bash -ex -c "
 rustup default nightly
 for app in $(echo app/*); do
     pushd \$app
@@ -21,6 +24,19 @@ for app in $(echo app/*); do
     popd
 done
 "
+        ;;
+        osx)
+            curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=nightly
+            brew cask install gcc-arm-embedded
+
+            for app in $(echo app/*); do
+                pushd $app
+                xargo build --target $target
+                arm-none-eabi-objdump -Cd -j .vector_table -j .text target/$target/debug/app
+                popd
+            done
+        ;;
+    esac
 }
 
 build_docs() {
