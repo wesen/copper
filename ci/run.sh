25f33ae6344e10bfa5d4904859f81a7c08eb89a4
Jorge Aparicio <japaricious@gmail.com>
2016-11-11 17:00:14 -0500

    trust-ify

---
+++ ci/run.sh
@@ -0,0 +1,30 @@
+set -ex
+
+test_mode() {
+    for app in $(echo app/*); do
+        local target=
+        case $app in
+            app/01-qemu)
+                target=thumbv7m-none-eabi
+                ;;
+            *)
+                target=thumbv7em-none-eabihf
+                ;;
+        esac
+
+        cd $app
+        xargo build --target $target
+        arm-none-eabi-objdump -Cd ${CARGO_TARGET_DIR:-target}/$target/debug/app
+        cd ../..
+    done
+
+    if [ $TRAVIS_OS_NAME = linux ]; then
+        mdbook build
+    fi
+}
+
+run() {
+    test_mode
+}
+
+run
